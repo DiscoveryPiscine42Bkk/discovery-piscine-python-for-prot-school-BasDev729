dfe1723c6d7d8096c86c8ab6435e8a04e67bd013
BasDev729 <tokkapongsi@gmail.com>
2025-03-10 14:56:50 +0700

    ex03

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,15 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+result = num1 * num2
+
+if result > 0:
+    print("The result is positive.")
+elif result < 0:
+    print("The result is negative.")
+else:
+    print("The result is zero.")
+
+print("The result of multiplying the two numbers is:", result)
+
+
