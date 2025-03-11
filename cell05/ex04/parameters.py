554f4d04e85ec91a2c0e3936cc7d3d62467bd4d4
BasDev729 <tokkapongsi@gmail.com>
2025-03-11 09:22:42 +0700

    ex04

---
+++ cell05/ex04/parameters.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+
+# Get the number of parameters (excluding the script name)
+num_params = len(sys.argv) - 1
+
+# Print the number of parameters followed by a newline
+print(num_params, "\n")
