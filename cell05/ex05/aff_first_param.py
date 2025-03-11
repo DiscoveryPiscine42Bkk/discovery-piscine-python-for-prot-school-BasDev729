21aaf4d97574486850985e7a90dc635b34ef555d
BasDev729 <tokkapongsi@gmail.com>
2025-03-11 09:23:07 +0700

    ex05

---
+++ cell05/ex05/aff_first_param.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+
+# Check if there are any parameters
+if len(sys.argv) > 1:
+    print(sys.argv[1])  # Print the first parameter
+else:
+    print("none")  # Print "none" if no parameters
